8f1ac1ef43eaee3669332f8292173406976502d6
Tracy Torisky <mommers1@verizon.net>
2021-12-01 15:55:05 -0500

    updated routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,6 +3,12 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const router = require('../routes');
+
+router.use('/product', productRoutes);
+router.use('/category', categoryRoutes);
+router.use('/tag', tagRoutes);
+router.use('/productTag', productTagRoutes);
 
 // Products belongsTo Category
 
